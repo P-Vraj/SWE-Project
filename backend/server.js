db39dd8f54895c1ae29391310e86bb6acffc1cd6
Vraj Patel <108849348+P-Vraj@users.noreply.github.com>
2024-03-25 16:56:30 -0400

    Small changes to server.js

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,17 @@ const mysql = require('mysql');
 const config = require('./config');
 
 const app = express();
-const port = 3001; // You can change this to any port you prefer
+const port = 3001;
 
 const User = require('./User');
 const Project = require('./Project');
 
-// Create a connection
 const db = mysql.createConnection(config);
 
 // Middleware to parse JSON bodies
 app.use(express.json());
 
+// IGNORE
 // Route to get data from a table
 app.get('/api/data', (req, res) => {
   db.query('SELECT * FROM your_table_name', (error, results) => {
@@ -26,6 +26,7 @@ app.get('/api/data', (req, res) => {
   });
 });
 
+// IGNORE
 // Route to add new data to the table
 app.post('/api/data', (req, res) => {
   const newData = req.body;
@@ -44,17 +45,19 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
+// Can run into race conditions sometimes
 async function test() {
-  var u1 = new User("Vraj", "ADMIN");
-  var u2 = new User("Shikha", "MANAGER");
-  var u3 = new User("Allen", "EMPLOYEE");
+  var u1 = new User("U1", "ADMIN");
+  var u2 = new User("U2", "MANAGER");
+  var u3 = new User("U3", "EMPLOYEE");
   try {
     await u1.addUser();
     await u2.addUser();
     await u3.addUser();
-    var p1 = new Project("A", "D1");
+    var p1 = new Project("P1", "Desc1");
     await p1.addProject();
     await p1.assignMember(u2);
+    await User.deleteUser(u2.user_id);
   } catch (err) {
     console.error(err);
   }
